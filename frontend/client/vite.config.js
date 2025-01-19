4ef3114037aca8105126d47eb3e976fc98b1f300
Hamza Dev <devhamzak@gmail.com>
2025-01-19 06:03:13 +0500

    p

--- frontend/client/vite.config.js
+++ frontend/client/vite.config.js
@@ -1,8 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
+const isProd = process.env.NODE_ENV === "production";
+
+/**
+ *  @type {import('vite').UserConfig}
+ */
+const config = defineConfig({
   plugins: [react()],
+  build:{
+    outDir: "../../api/src/public/dist",
+  },
+  server: {
+    host: "localhost",
+    proxy: {
+      "/api": {
+        target: "http://localhost:8801",
+        changeOrigin: true,
+        secure: false,
+        logLevel: "debug",
+      },
+    },
+  },
 });
- 
+
+if(isProd) {
+  delete config.server;
+}
+
+export default config;
