07d2fe9e60e6293c3c9f5fec161e26008685ce61
anasnaeem1 <clownop473@gmail.com>
2025-02-01 16:59:39 +0500

    search added for mobile

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
-const cors = require("cors");
+// const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const path = require("path");
