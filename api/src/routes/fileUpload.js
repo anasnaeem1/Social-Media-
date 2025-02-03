c5e305c2c970067a7c3cea81fa8e0fa7e0e732d0
anasnaeem1 <clownop473@gmail.com>
2025-02-03 05:12:47 +0500

    k

--- api/src/routes/fileUpload.js
+++ api/src/routes/fileUpload.js
@@ -7,36 +7,22 @@ dotenv.config();
 
 const router = express.Router();
 
-const isCloudinaryConfigured = () => {
-  return (
-    process.env.CLOUDINARY_CLOUD_NAME &&
-    process.env.CLOUDINARY_API_KEY &&
-    process.env.CLOUDINARY_API_SECRET
-  );
-};
-
-if (!isCloudinaryConfigured()) {
-  console.error(
-    "Cloudinary configuration is missing. Please check your .env file."
-  );
-}
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: "datcr1zua",
+  api_key: "252361716913516",
+  api_secret: "iIeFjHzkowTaCSN07ygz5J7D7rM",
 });
 
 router.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" }));
 
 router.post("/uploads", async (req, res) => {
   try {
-    // if (!isCloudinaryConfigured()) {
-    //   return res
-    //     .status(500)
-    //     .json({ error: "Cloudinary configuration is missing" });
-    // }
-
     if (!req.files || Object.keys(req.files).length === 0) {
       return res.status(400).json({ error: "No file uploaded" });
     }
@@ -44,12 +30,10 @@ router.post("/uploads", async (req, res) => {
     const file = req.files.file;
     console.log("Uploaded file:", file);
 
-    if (isCloudinaryConfigured()) {
       const result = await cloudinary.uploader.upload(file.tempFilePath, {
         folder: "uploads",
       });
       res.status(200).json({ url: result.secure_url });
-    }
   } catch (error) {
     console.error("Cloudinary Upload Error:", error);
     res.status(500).json({ error: "Upload failed", details: error.message });
